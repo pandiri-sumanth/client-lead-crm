6df61a2e93b3156563e0275f745c14a2d923771f
PANDIRI SUMANTH <pandirisumanth@gmail.com>
2026-05-29 11:48:39 +0530

    Update MySQL connection for Aiven

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,8 @@ CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mysql+pymysql://{os.getenv('MYSQL_USER')}:"
     f"{os.getenv('MYSQL_PASSWORD')}@"
-    f"{os.getenv('MYSQL_HOST')}/"
+    f"{os.getenv('MYSQL_HOST')}:"
+    f"{os.getenv('MYSQL_PORT')}/"
     f"{os.getenv('MYSQL_DB')}"
 )
 
