49742e5270731e42c8335848f18357342ea2a5fb
pandiri-sumanth <pandirisumanth@gmail.com>
2026-05-29 12:06:00 +0530

    Update leadService.js

--- frontend/src/services/leadService.js
+++ frontend/src/services/leadService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://127.0.0.1:5000"
+const API_URL = "https://client-lead-crm-backend.onrender.com"
 
 
 export const addLead = async (leadData) => {
